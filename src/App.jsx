42cf8e1eab3e6189ea6da73fa26ef981aed2e2fc
maisonmake <guibert.axelle@gmail.com>
2026-05-30 11:41:37 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import MealIndex from "./MealIndex";
-import Week20260525 from "./weeks/Week20260525";
+import WeekPage from "./weeks/WeekPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MealIndex />} />
-      <Route path="/weeks/2026-05-25" element={<Week20260525 />} />
+      <Route path="/weeks/:weekId" element={<WeekPage />} />
     </Routes>
   );
 }
